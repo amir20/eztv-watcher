6bacc80dbf94995234b34c1ff8adc642739b3594
Amir Raminfar <findamir@gmail.com>
2018-08-24 19:42:35 -0700

    Adds database

---
+++ database.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/gob"
+	"os"
+)
+
+// Database represent a map on disk
+type Database interface {
+	CreateIfMissing() error
+	Load() error
+	Save() error
+	UpdateValue(string, int)
+	GetValue(string) (int, bool)
+}
+
+type database struct {
+	path string
+	data *map[string]int
+}
+
+// NewDatabase creates a new instance of in memory database
+func NewDatabase(path string) Database {
+	db := make(map[string]int)
+	return &database{path, &db}
+}
+
+func (db *database) CreateIfMissing() error {
+	if _, err := os.Stat(db.path); os.IsNotExist(err) {
+		return db.Save()
+	}
+
+	return nil
+}
+
+func (db *database) Load() error {
+	file, err := os.Open(db.path)
+
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(db.data)
+	}
+
+	file.Close()
+
+	return err
+}
+
+func (db *database) Save() error {
+	file, err := os.Create(db.path)
+
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(db.data)
+	}
+
+	file.Close()
+
+	return err
+}
+
+func (db *database) UpdateValue(k string, v int) {
+	(*db.data)[k] = v
+}
+
+func (db *database) GetValue(k string) (int, bool) {
+	v, ok := (*db.data)[k]
+	return v, ok
+}
--- main.go
+++ main.go
@@ -1,11 +1,82 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
+	"os"
+
 	"github.com/amir20/eztv-watcher/eztv"
+	"github.com/spf13/viper"
 )
 
+func loadDb(path string, object interface{}) error {
+	file, err := os.Open(path)
+	if err != nil {
+		err = saveDb(path, object)
+
+		if err != nil {
+			panic(fmt.Errorf("fatal error creating database: %s", err))
+		}
+
+		return nil
+	}
+
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(object)
+	}
+
+	file.Close()
+
+	return err
+}
+
+func saveDb(path string, object interface{}) error {
+	file, err := os.Create(path)
+
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(object)
+	}
+
+	file.Close()
+
+	return err
+}
+
 func main() {
-	response, _ := eztv.FetchTorrents("6048596")
-	fmt.Printf("%+v\n", response)
+	viper.SetConfigName("config")
+	viper.AddConfigPath("$HOME/.config/eztv")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error reading config file: %s", err))
+	}
+
+	database := NewDatabase(viper.GetString("database.path"))
+	err = database.CreateIfMissing()
+	if err != nil {
+		panic(fmt.Errorf("fatal error creating database: %s", err))
+	}
+
+	err = database.Load()
+	if err != nil {
+		panic(fmt.Errorf("fatal error loading database: %s", err))
+	}
+
+	for _, id := range viper.GetStringSlice("ids") {
+		response, _ := eztv.FetchTorrents(id)
+		fmt.Printf("%+v\n", response.Torrents[0].Title)
+		if lastID, ok := database.GetValue(id); ok {
+			println(lastID)
+		}
+
+		database.UpdateValue(id, response.Torrents[0].ID)
+	}
+
+	err = database.Save()
+	if err != nil {
+		panic(fmt.Errorf("fatal error writing database: %s", err))
+	}
+
 }
